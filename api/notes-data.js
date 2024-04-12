42143b815ef7d8b0110db87937ce47be61631c7c
Daniel Rubio <drubio432@gmail.com>
2024-04-11 21:12:29 -0400

    using module exports

---
+++ api/notes-data.js
@@ -0,0 +1,18 @@
+const notes = {
+  tkwvorm0: {
+    title: "Note 1",
+    content: "this is note 1",
+    id: "tkwvorm0",
+    createdAtDate: "4/11/2024, 8:32:20 PM",
+    updatedAtDate: "4/11/2024, 8:32:29 PM",
+  },
+  yxvnkxuk: {
+    title: "Note 2",
+    content: "this is another note",
+    id: "yxvnkxuk",
+    createdAtDate: "4/11/2024, 8:32:32 PM",
+    updatedAtDate: "4/11/2024, 8:32:40 PM",
+  },
+};
+
+module.exports = notes;
--- api/server.js
+++ api/server.js
@@ -1,23 +1,7 @@
 const http = require("http");
+const notes = require("./notes-data");
 const host = "localhost";
 const port = 8000;
-
-const notes = {
-  tkwvorm0: {
-    title: "Note 1",
-    content: "this is note 1",
-    id: "tkwvorm0",
-    createdAtDate: "4/11/2024, 8:32:20 PM",
-    updatedAtDate: "4/11/2024, 8:32:29 PM",
-  },
-  yxvnkxuk: {
-    title: "Note 2",
-    content: "this is another note",
-    id: "yxvnkxuk",
-    createdAtDate: "4/11/2024, 8:32:32 PM",
-    updatedAtDate: "4/11/2024, 8:32:40 PM",
-  },
-};
 
 const requestListener = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
